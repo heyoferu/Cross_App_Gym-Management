1c4b966fee4a8af36d08040fb0020f3b0a54cf95
heyoferu <heyoferu@users.noreply.github.com>
2024-05-28 20:58:13 -0600

    feat(view models): set content view model

--- App/ViewModels/MainWindowViewModel.cs
+++ App/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,21 @@
-﻿namespace App.ViewModels;
+﻿using App.Services;
+using ReactiveUI;
+
+namespace App.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private ViewModelBase _contentViewModel;
+
+    public MainWindowViewModel()
+    {
+        var service = new LoginService();
+        _contentViewModel = new LoginViewModel(service);
+    }
+
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
 }
