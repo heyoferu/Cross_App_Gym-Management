6d64c42380ead2d176cb8f63da1a3df7a850958c
heyoferu <heyoferu@users.noreply.github.com>
2024-05-29 18:53:25 -0600

    feat(view models): add missing properties to hide or show token and login msg content

--- App/ViewModels/LoginViewModel.cs
+++ App/ViewModels/LoginViewModel.cs
@@ -4,4 +4,11 @@ public class LoginViewModel : ViewModelBase
 {
     public string? Username{ get; set; }
     public string? Password { get; set; }
+    
+    private string? _token = "";
+    private bool _isTokenVisible = false;
+    
+    private string? _loginMsg = "";
+    private bool _isLoginMsgVisible = false;
+    
 }
